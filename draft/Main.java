8fff71237521eb797ea94322f86440b05ca3efed
Gabriel Barros <gbarros1337@gmail.com>
2023-12-07 11:56:35 -0300

    Changes in the draft folder

---
+++ draft/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String args[]) {
+        Test myCar = new Test();
+        myCar.fullThrottle();
+        myCar.speed(200);
+    }
+}
---
+++ draft/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed: " + maxSpeed);
+    }
+
+    /*
+    public static void main(String args[]) {
+        Test myCar = new Test();
+        myCar.fullThrottle();
+    }
+    */
+}
